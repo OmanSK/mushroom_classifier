2c52f00588d29276bf7425fdec1fa81980bd3bdc
OmanSK <110732673+OmanSK@users.noreply.github.com>
2023-06-03 03:18:28 +0300

    main

---
+++ hello.py
@@ -0,0 +1,63 @@
+from aiogram import Bot, executor, Dispatcher, types
+from io import BytesIO
+from PIL import Image
+import requests
+from model import prediction as PRED
+from config import TOKEN_API
+
+bot = Bot(token=TOKEN_API)
+dp = Dispatcher(bot)
+
+URL_IMAGE = f'https://api.telegram.org/bot{TOKEN_API}/getFile?file_id='
+URL = f'https://api.telegram.org/file/bot{TOKEN_API}/'
+
+HELP_MESSAGE = """
+    Желательно, чтобы гриб располагался по центру.
+    В боте представлены только те грибы, что растут на территории Брянской области:
+    Бледная поганка
+    Боровик
+    Волнушка розовая
+    Груздь
+    Дождевик
+    Лиловка лиловоногая
+    Лисичка
+    Ложнодождевик
+    Ложноопенок серно-желтый
+    Моховик зеленый
+    Мухомор красный
+    Опенок осенний
+    Подберезовик
+    Подосиновик
+    Польский гриб
+    Рядовка тигровая
+    Сатанинискй гриб
+    Сморчок
+    Шампиньон желтокожий
+"""
+
+@dp.message_handler(commands=['start'])
+async def start_message(msg: types.Message):
+    await msg.answer('Выбери фото для классификации\nБот определяет только те грибы, которые указаны в //help')
+
+@dp.message_handler(commands=['help'])
+async def help_message(msg: types.Message):
+    await msg.answer(HELP_MESSAGE)
+
+@dp.message_handler(content_types=types.ContentType.PHOTO)
+async def process_photo(msg: types.Message):
+    print(msg.photo)
+    file = msg.photo[-1].file_id
+    resp = requests.get(URL_IMAGE + str(file))
+    image_path = resp.json()['result']['file_path']
+    img = requests.get(URL + image_path)
+    img = Image.open(BytesIO(img.content)).convert('RGB')
+    
+    result = PRED(img)
+    answer = ''
+    for k, v in result.items():
+        answer += k.capitalize() + ': ' + v + '\n'
+    
+    await msg.reply(text=answer)
+    
+if __name__=='__main__':
+    executor.start_polling(dispatcher=dp, skip_updates=True)
---
+++ model.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms as T
+from torchvision.models import resnet50
+import onnxruntime as ort
+from config import ONNX
+
+
+device = 'cpu'
+dictionary = {
+    'бледная поганка': 0,
+    'боровик': 1,
+    'волнушка розовая': 2,
+    'груздь': 3,
+    'дождевик': 4,
+    'лиловка лиловоногая': 5,
+    'лисичка': 6,
+    'ложнодождевик': 7,
+    'ложноопенок серно-желтый': 8,
+    'моховик зеленый': 9,
+    'мухомор красный': 10,
+    'опенок осенний': 11,
+    'Подберезовик': 12,
+    'Подосиновик': 13,
+    'польский гриб': 14,
+    'рядовка тигровая': 15,
+    'сатангриб': 16,
+    'сморчок': 17,
+    'шампиньон желтокожий': 18
+ }
+reverse_dictionary = {value : key for key, value in dictionary.items()}
+
+
+def prediction(image_pil):
+
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    transforms = T.Compose([
+        T.Resize(size=(128,128)),
+        T.PILToTensor(),
+        T.ConvertImageDtype(torch.float),
+        T.ColorJitter(brightness=(0.9, 1.2), contrast=(0.8, 1.2), hue=(-0.01, 0.01)),
+        T.RandomRotation(degrees=5),
+        T.RandomHorizontalFlip(p=0.5),
+        T.RandomErasing(p=0.4, ratio=(0.1, 0.2)),
+        T.Normalize(mean=mean, std=std),
+    ])
+
+    image_tensor = transforms(image_pil)
+    #prepare image to onnx
+    image_tensor = image_tensor.unsqueeze(0).detach().cpu().numpy()
+
+    print(image_tensor.shape)
+
+    ort_session = ort.InferenceSession(ONNX)
+    ort_inputs = {ort_session.get_inputs()[0].name: image_tensor}
+    ort_outputs = ort_session.run(None, ort_inputs)[0][0]
+    softmax = nn.Softmax(dim=0)(torch.tensor(ort_outputs)).detach().numpy()
+    
+    probability = [(name,proba) for name, proba in enumerate(softmax)]
+    probability = sorted(probability, key=lambda x: x[-1], reverse=True)[:3]
+
+    answer = {}
+    for element in probability:
+        answer[reverse_dictionary[element[0]]] = f'Вероятность - {round(element[-1]* 100, 4)}%'
+
+    return answer
+
+
+
