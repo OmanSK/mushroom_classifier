69f8512a802844e819d7cbc315a0870b00b25f6c
OmanSK <110732673+OmanSK@users.noreply.github.com>
2023-06-06 00:41:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ TOKEN_API = '5755368494:AAHNux-1u6B62xuh65p367_t7x1K2nsi3Pk'
 BOT_NAME = 'MushroomClassificationBot'
 bot_name_create = 'mushroom_classification_bot'
 
-ONNX = './weights/mushmodel.onnx'
+ONNX = 'weights/mushmodel.onnx'
